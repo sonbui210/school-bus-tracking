eb1e6a6123a74aac13c92fd012b0c078d632a764
Sơn Bùi <79527137+sonbui210@users.noreply.github.com>
2021-07-03 10:23:02 +0700

    Update googleMap.js
    hellokitty

--- src/lib/googleMap.js
+++ src/lib/googleMap.js
@@ -4,7 +4,7 @@ import { app } from "./app";
 var initMap = function() {
   // Create the script tag, set the appropriate attributes
   var script = document.createElement("script");
-  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyBdKgzYknfDYqA-oPi1YOaQUvadM-1ZRSY&region=VN&language=vn&callback=initMap";
+  script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyDJVB0hTqJMdb1rIilxAAMEbcOcWioisg8&region=VN&language=vn&callback=initMap";
   script.defer = true;
 
   // Attach your callback function to the `window` object
